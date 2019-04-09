472219a76257b1b6c2f76021665327f7344b49ef
Yan Podkorytoff <yan.podkorytoff@gmail.com>
2019-04-09 18:55:08 -0300

    Delete estimation.m

--- estimation.m
+++
@@ -1,6 +0,0 @@
-function chanest = estimation(data, rec_data, L)
-        xmtx = convmtx(data, L); 
-        xmtx(end-L+2:end, :) = []; 
-        pinv_xmtx = pinv(xmtx); 
-        chanest = pinv_xmtx*rec_data; 
-       

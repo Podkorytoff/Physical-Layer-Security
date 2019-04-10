df1cdb6675cc1fd24380c14ede26d62a598772b6
Yan Podkorytoff <yan.podkorytoff@gmail.com>
2019-04-10 16:01:17 -0300

    Função 1 para o teste de aleatoriedade
    Frequency monobit test

---
+++ frequency_bit_test.m
@@ -0,0 +1,13 @@
+function p_value = frequency_bit_test(key)
+    
+    for idx = 1:length(key)
+        if(key(idx) == 0)
+            key(idx) = -1;
+        end
+    end
+    S = sum(key)/sqrt(length(key));
+    p_value = erfc(S/sqrt(2));
+    
+    
+  
+            

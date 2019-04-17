474a6c1a0953b66f22f365f54afa99c3acc868a4
Yan Podkorytoff <yan.podkorytoff@gmail.com>
2019-04-17 00:34:05 -0300

    Conserto
    Incomplete gamma function estava sendo usada de forma errada.

--- frequency_block_test.m
+++ frequency_block_test.m
@@ -1,11 +1,11 @@
 function p_value = frequency_block_test(key, bloco)
 %     A distribuição de referência para este teste é o X²(chi-square)
-%     key = [1 1 0 0 1 0 0 1 0 0 0 0 1 1 1 1 1 1 0 1 1 0 1 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 0 1 0 1 1 0 1 0 0 0 1 1 0 0 0 0 1 0 0 0 1 1 0 1 0 0 1 1 0 0 0 1 0 0 1 1 0 0 0 1 1 0 0 1 1 0 0 0 1 0 1 0 0 0 1 0 1 1 1 0 0 0];
-%     bloco = 10;  
+    key = [1 1 0 0 1 0 0 1 0 0 0 0 1 1 1 1 1 1 0 1 1 0 1 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 0 1 0 1 1 0 1 0 0 0 1 1 0 0 0 0 1 0 0 0 1 1 0 1 0 0 1 1 0 0 0 1 0 0 1 1 0 0 0 1 1 0 0 1 1 0 0 0 1 0 1 0 0 0 1 0 1 1 1 0 0 0];
+    bloco = 10;  
     N = length(key)/bloco;
     key = reshape(key, [bloco, N]);
     proportion = (sum(key)/bloco - 1/2).^2;
     chi_square = 4*bloco*sum(proportion);
-    p_value = gammainc(N/2,chi_square/2);
+    p_value = 1 - gammainc(chi_square/2,N/2);
         
     
